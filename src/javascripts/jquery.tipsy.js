063dc5810361f92985f6d05d8ed4ca93484a9445
Christian Boenning <christian@verloren-im.net>
2010-05-18 21:27:20 +0200

    Don't quote maxWitdh. It's an integer.

--- src/javascripts/jquery.tipsy.js
+++ src/javascripts/jquery.tipsy.js
@@ -95,7 +95,7 @@
         
         tip: function() {
             if (!this.$tip) {
-                this.$tip = $('<div class="tipsy"></div>').html('<div class="tipsy-arrow"></div><div class="tipsy-inner" style="max-width:'+this.options.maxWidth+'px"/></div>');
+                this.$tip = $('<div class="tipsy"></div>').html('<div class="tipsy-arrow"></div><div class="tipsy-inner" style="max-width:' + this.options.maxWidth + 'px"/></div>');
             }
             return this.$tip;
         },
@@ -177,7 +177,7 @@
         opacity: 0.8,
         title: 'title',
         trigger: 'hover',
-        maxWidth: '250'
+        maxWidth: 250
     };
     
     // Overwrite this method to provide options on a per-element basis.
